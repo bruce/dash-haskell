47e1e59e73ea3a00e6b59eeab6545bd77af6a898
Bruce Williams <bruce@codefluency.com>
2009-03-09 14:04:43 -0500

    Add exposed-modules setting to .cabal so `cabal haddock` will give us pretty docs

--- Web/Dash.hs
+++ Web/Dash.hs
@@ -1,2 +1,18 @@
+{-# OPTIONS_GHC -XCPP -XMultiParamTypeClasses -XTypeSynonymInstances #-}
+--------------------------------------------------------------------
+-- |
+-- Module    : Web.Dash
+-- Copyright : (c) 2009 Bruce Williams, Adam Keys
+-- License   : BSD3
+--
+-- Maintainer:  Bruce Williams <bruce@codefluency.com>, Adam Keys <adam@therealadam.com>
+-- Stability :  provisional
+-- Portability: portable
+--
+--------------------------------------------------------------------
+--
+-- Provides an API for Haskell applications to the FiveRuns Dash service,
+-- http://dash.fiveruns.com
+--
 module Web.Dash (fetch, fetch') where
   import Web.Dash.Fetch
